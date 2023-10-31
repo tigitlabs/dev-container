ac7ed4a51acc4b440a4e2f1711371e3e42a4f950
Tigit Labs <61022457+tigitlabs@users.noreply.github.com>
2023-10-31 16:23:33 +0000

    wip: added docker build smoke test

---
+++ .github/actions/smoke-test-docker/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+IMAGE="$1"
+
+set -e
+
+export DOCKER_BUILDKIT=1
+
+echo "(*) Building image - ${IMAGE}"
+image_tag="ci-image-${IMAGE}"
+echo $image-tag
+docker build --tag $image_tag --file "src/${IMAGE}/.devcontainer/Dockerfile" "src/${IMAGE}/.devcontainer/"
---
+++ .github/actions/smoke-test-docker/test.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+IMAGE="$1"
+
+export DOCKER_BUILDKIT=1
+set -e
+
+# Run actual test
+echo "(*) Running test..."
+image_tag="ci-image-${IMAGE}"
+
+# devcontainer exec 
+# --workspace-folder $(pwd)/src/$IMAGE  
+# --id-label ${id_label} 
+# /bin/sh -c 
+# 'set -e && 
+# if [ -f "test-project/test.sh" ]; then 
+#    cd test-project 
+#    && if [ "$(id -u)" = "0" ]; then 
+#         chmod +x test.sh;
+#      else 
+#         sudo chmod +x test.sh;
+#      fi 
+#  && ./test.sh; 
+# else 
+#   ls -a; fi'
+# Run the container and execute the test script
+docker run \
+--rm \
+--label ${image_tag} \
+--volume $(pwd)/src/$IMAGE:/workspace \
+--workdir /workspace \
+$image_tag /bin/sh -c 'set -e && if [ -f "test-project/test-docker.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test-docker.sh; else sudo chmod +x test-docker.sh; fi && ./test-docker.sh; else ls -a; fi'
+
+echo "(*) Docker image details..."
+docker images
+
+# Clean up
+docker rm -f $(docker container ls -f "label=${id_label}" -q)

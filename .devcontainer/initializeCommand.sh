7450fb2fd463ab68c9280295ea58b0b147c11ece
Timur <timur@tigitlabs.com>
2023-11-15 13:55:57 +0000

    feat: login to ghcr and github cli

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # .devcontainer/initializeCommand.sh
-
+echo "🏗️ Initialize command"
 # Check if the script is running on GitHub Codespaces
 if [[ -n "${CODESPACES}" || -n "${GITHUB_CODESPACE_TOKEN}" ]]
 then
--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 # .devcontainer/postStartCommand.sh
-echo "Post start command"
+echo "🏗️ Post create command"
 # Check if the script is running on GitHub Codespaces
 if [[ -n "${CODESPACES}" || -n "${GITHUB_CODESPACE_TOKEN}" ]]; then
     printf "Running in GitHub Codespaces.\nNo need to run any commands."
     exit 0
 else
-    ENV_FILE=".devcontainer/.env"
     echo "Running on local host"
-    echo "Load environment variables from ${ENV_FILE}"
-    if [ -f "$ENV_FILE" ]; then
-        # Get absolute path of .env file
-        ENV_FILE=$(realpath ${ENV_FILE})
-        echo "Add env file ${ENV_FILE} to ~/.bashrc"
-        echo "### Load env variables" >> ~/.bashrc
-        echo "# Added from postStartCommand.sh" >> ~/.bashrc
-        echo "set -o allexport" >> ~/.bashrc
-        echo "source ${ENV_FILE}" >> ~/.bashrc
-        echo "set +o allexport" >> ~/.bashrc
-        echo "Login to github.com with GITHUB_TOKEN"
-        echo "### Load env variables" >> ~/.bashrc
-    else
-        echo "$ENV_FILE does not exist."
-        exit 1
-    fi
+    set -o allexport
+    source .devcontainer/.env
+    set +o allexport
+    echo "Login status for github cli"
+    gh auth status
+    echo "Login docker to ghcr.io for: ${GITHUB_USER}"
+    echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin
 fi
